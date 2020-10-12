24b021aaeabf242364e644824337556f05a87eca
Winnie Otieno <winnieotieno770@gmail.com>
2020-10-12 23:37:02 +0300

    Completed module

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,7 +1,11 @@
 import { Track } from './track';
+
+
 export interface Album {
-    name:string;
-    releaseDate:string;
-    coverImage:string;
-    tracks: Track [];
+  name: string;
+  releaseDate: string;
+  coverImage: string;
+  tracks: Track [];
+   
+  
 }
