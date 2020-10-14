b4f272d9a7016082c4416ad2b9019f23bdc85cd3
Winnie Otieno <winnieotieno770@gmail.com>
2020-10-14 10:41:25 +0300

    complete module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -12,6 +12,6 @@ export class ProductTracklistingComponent implements OnInit {
   ngOnInit() {
     this._productService
       .getAlbum(1)
-      .subscribe((response) => (this.albumInfo = response));
+      .subscribe(response =>this.albumInfo = response);
   }
 }
